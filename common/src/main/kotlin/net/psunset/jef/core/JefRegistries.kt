1a1eab8781546300dc724de571ce443fd93bf042
NoodleInWater <why097729@gmail.com>
2026-02-08 20:25:02 +0800

    Relax the restriction of API

--- common/src/main/kotlin/net/psunset/jef/core/JefRegistries.kt
+++ common/src/main/kotlin/net/psunset/jef/core/JefRegistries.kt
@@ -1,19 +1,21 @@
 package net.psunset.jef.core
 
 import net.psunset.jef.api.IFilterProxy
+import net.psunset.jef.api.IItemTypeFilter
+import net.psunset.jef.api.IToggledFilter
 
 object JefRegistries {
     @JvmField
-    internal val TOGGLED_FILTERS = mutableMapOf<String, ToggledFilter>()
+    internal val TOGGLED_FILTERS = mutableMapOf<String, IToggledFilter>()
 
     @JvmField
-    internal val ITEM_TYPE_FILTERS = mutableListOf<Pair<String, ItemTypeFilter>>()
+    internal val ITEM_TYPE_FILTERS = mutableListOf<Pair<String, IItemTypeFilter>>()
 
     @JvmField
     internal val PROXIES = mutableListOf<IFilterProxy>()
 
     @JvmStatic
-    fun registerToggledFilter(filter: ToggledFilter) {
+    fun registerToggledFilter(filter: IToggledFilter) {
         if (TOGGLED_FILTERS.containsKey(filter.id.toString())) {
             throw IllegalStateException("ToggledFilter with id ${filter.id} is already registered.")
         }
@@ -21,12 +23,12 @@ object JefRegistries {
     }
 
     @JvmStatic
-    internal fun registerItemTypeFilter(index: Int, filter: ItemTypeFilter) {
+    internal fun registerItemTypeFilter(index: Int, filter: IItemTypeFilter) {
         ITEM_TYPE_FILTERS.add(index, filter.id.toString() to filter)
     }
 
     @JvmStatic
-    fun registerItemTypeFilter(filter: ItemTypeFilter) {
+    fun registerItemTypeFilter(filter: IItemTypeFilter) {
         if (ITEM_TYPE_FILTERS.any { it.first == (filter.id.toString()) } ) {
             throw IllegalStateException("ItemTypeFilter with id ${filter.id} is already registered.")
         }
