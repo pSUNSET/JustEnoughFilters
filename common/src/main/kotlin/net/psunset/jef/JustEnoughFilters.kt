2a48594510ab2557de35648a7f1a3d7372a46a5c
NoodleInWater <why097729@gmail.com>
2026-06-15 00:36:28 +0800

    feat: add deferred registration function to api

--- common/src/main/kotlin/net/psunset/jef/JustEnoughFilters.kt
+++ common/src/main/kotlin/net/psunset/jef/JustEnoughFilters.kt
@@ -81,5 +81,6 @@ object JustEnoughFilters {
         ToggledFilters.init()
         ItemTypeFilters.init()
         ConfigManager.onLoading()
+        JefRegistries.registerDeferredElements()
     }
 }
