bef6c592c18cf70386e6f0bc8092fd4f74b58008
NoodleInWater <why097729@gmail.com>
2026-06-21 01:52:30 +0800

    improv: update DeferredElement

--- common/src/main/kotlin/net/psunset/jef/registry/DeferredElement.kt
+++ common/src/main/kotlin/net/psunset/jef/registry/DeferredElement.kt
@@ -3,19 +3,27 @@ package net.psunset.jef.registry
 import java.util.function.Supplier
 import kotlin.reflect.KProperty
 
-class DeferredElement<T : Any>(val supplier: Supplier<T>) : Function0<T>, Supplier<T> {
-    private var value: T? = null
-    val nonnullValue get() = value!!
+/**
+ * A value wrapper, making initialization of the element deferred.
+ *
+ * Implements [kotlin.jvm.functions.Function0] and [Supplier].
+ *
+ * Operator function [getValue] is also available,
+ * meaning get deferred value with `by` keyword is supported.
+ */
+class DeferredElement<T : Any>(private val supplier: Supplier<T>) : Function0<T>, Supplier<T> {
+
+    val value: T by lazy { supplier.get() }
 
     override fun invoke(): T {
-        return value ?: supplier.get().also { value = it }
+        return value
     }
 
     override fun get(): T {
-        return invoke()
+        return value
     }
 
     operator fun getValue(any: Any?, property: KProperty<*>): T {
-        return invoke()
+        return value
     }
 }
